01cfa217b7cc1b65600e5a3e0955bda9b910c7c2
Niranjan <niranjan.v.94@gmail.com>
2022-08-25 19:22:54 +0530

    Added the functionality so it can work completely offline solving caching problem.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const currentTask = process.env.npm_lifecycle_event
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const config = {
     entry: "./app/app.js",
@@ -13,7 +14,12 @@ const config = {
     plugins: [new HtmlWebpackPlugin({
         template: "./app/index.html",
         inject: "body"
-    })],
+    }),
+    new WorkboxPlugin.GenerateSW({
+        clientsClaim: true,
+        maximumFileSizeToCacheInBytes: 10000000,
+        skipWaiting: true
+    }),],
     module: {
         rules: [
             {
@@ -30,7 +36,7 @@ const config = {
         host: "0.0.0.0",
         port: 8080,
         allowedHosts: "all",
-        static: path.resolve(__dirname, "dist"),
+        static: path.resolve(__dirname, "docs"),
         hot: true
     },
     devtool: false,
